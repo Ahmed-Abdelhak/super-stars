16e40e316fc5abd5e9eff1ab6fbc6d160da1c238
aabdelhak <ahmed.abdelhak@trivago.com>
2024-04-14 21:53:21 +0200

    [Task-id] removed abstraction for fetching after implementing client abstraction

--- super-github/src/main/kotlin/fetcher/FetcherInterface.kt
+++
@@ -1,7 +0,0 @@
-package com.redcare.pharmacy.fetcher
-
-import com.redcare.pharmacy.common.SuperModelInterface
-
-interface FetcherInterface<TModel: SuperModelInterface> {
-    fun fetch(): List<TModel>
-}
--- super-github/src/main/kotlin/validation/ValidatorInterface.kt
+++
@@ -1,7 +0,0 @@
-package com.redcare.pharmacy.validation
-
-import com.redcare.pharmacy.common.SuperModelInterface
-
-interface ValidatorInterface<TModel: SuperModelInterface> {
-    fun isValid(model: TModel): Boolean
-}
