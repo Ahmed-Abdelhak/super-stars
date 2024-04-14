34820d3256e4b689241eff71fb78d79c9b72a4ec
aabdelhak <ahmed.abdelhak@trivago.com>
2024-04-14 22:54:52 +0200

    [Task-id] implemented main service

---
+++ super-github/src/main/kotlin/main/GithubService.kt
@@ -0,0 +1,29 @@
+package com.redcare.pharmacy.main
+
+import com.redcare.pharmacy.common.model.GithubRepositoryModel
+import com.redcare.pharmacy.error_handler.ErrorHandlerInterface
+import com.redcare.pharmacy.filter.FilterInterface
+import com.redcare.pharmacy.integration.client.http.GithubHttpClient
+import com.redcare.pharmacy.integration.client.http.HttpClientError
+import org.springframework.stereotype.Service
+
+@Service
+class GithubService(
+    private val githubClient: GithubHttpClient,
+    private val filter: FilterInterface<GithubRepositoryModel>,
+    private val errorHandler: ErrorHandlerInterface<HttpClientError>
+) {
+
+    fun getPopularRepositories()  =
+        githubClient.getRepositories()
+            .let { response ->
+                if (response.isSuccess) {
+                    response.modelList.filter {
+                        filter.filter(it)
+                    }
+                } else {
+                    errorHandler.handle(response.error!!)
+                }
+            }
+}
+
